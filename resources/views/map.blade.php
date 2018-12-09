1c7823fd245289e0a1cb43a8ee3dc9ca84c589e0
hendrywiranto <hendrywiranto24@gmail.com>
2018-12-09 18:27:57 +0700

    balik x ama y

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -11,19 +11,19 @@
     var img;
 var img2;
 function preload() {
-    img = loadImage('img/icon.png');
-    img2 = loadImage('img/ajk.png');
+    img = loadImage('{{ asset('img/icon.png')}}');
+    img2 = loadImage('{{ asset('img/ajk.png')}}');
 }
 function setup() {
 //   background(50);
 // Top-left corner of the img is at (10, 10)
 // Width and height are 50 x 50
-createCanvas(1000, 1000);
+createCanvas(1100, 1100);
 }
 
 function draw(){
     background(img2);
-    image(img, 0, 0, 50, 50);
+    image(img, {{ $x * 100 }}, {{ $y * 100 }}, 50, 50);
 }
 </script>
 </head>
